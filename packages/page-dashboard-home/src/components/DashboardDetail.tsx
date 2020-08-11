515d411181b378812e8cc8b847c13eb11ef1f9f5
aniiantt <nsso@foxmail.com>
2020-08-12 11:43:48 +1200

    chart

--- packages/page-dashboard-home/src/components/DashboardDetail.tsx
+++ packages/page-dashboard-home/src/components/DashboardDetail.tsx
@@ -1,94 +1,128 @@
-import React, { FC, useMemo } from 'react';
 import { Statistic } from '@acala-dapp/ui-components';
-import { useRequest } from '@acala-dapp/react-hooks';
-
+import React, { FC } from 'react';
+import { Fixed18 } from '@acala-network/app-util';
 import classes from './DashboardDetail.module.scss';
-import { TinyAreaChart } from 'bizcharts';
-import { get } from 'lodash';
+import { useDashboard } from '@acala-dapp/react-hooks';
+import { TinyAreaChart, Chart, Area, Line, Tooltip, Axis, Coordinate } from 'bizcharts';
+
+interface StatisticContentProps {
+  value?: string;
+  history?: {
+    date: number;
+    value: number;
+  }[];
+  scale?: any;
+  areaColor: string;
+  lineColor: string;
+}
+
+const StatisticContent: FC<StatisticContentProps> = ({ areaColor, lineColor, scale, value, history }) => {
+  return (
+    <div className={classes.statisticContent}>
+      <div>{value || '--'}</div>
+      <div>
+        <TinyAreaChart
+          scale
+          color={lineColor}
+          data={history || []}
+          height={60}
+          width={124}
+          xField='date'
+          yField='value'
+        />
+      </div>
+    </div>
+  );
+};
 
 const DashboardDetail: FC = () => {
-  const aUSDIssuedData = useRequest(() => ({
-    method: 'GET',
-    url:
-      'http://39.99.168.67:8086/query?q=SELECT MAX("amount") FROM "acala"."autogen"."issuance" WHERE time > now() - 30d AND time < now() AND asset = \'AUSD\' GROUP BY time(1d)'
-  }));
-
-  const newAccountData = useRequest(() => ({
-    method: 'GET',
-    url:
-      'http://39.99.168.67:8086/query?q=SELECT SUM("count") FROM "acala"."autogen"."new-account" WHERE time > now() - 30d AND time < now() GROUP BY time(1d)'
-  }));
-
-  const [todayAUSD, aUSDIssuedHistory] = useMemo(() => {
-    if (!aUSDIssuedData.data || !aUSDIssuedData.data.results) return [];
-    const temp = get(aUSDIssuedData, 'data.results.0.series.0.values') as any[];
-    const today = get(temp, [temp.length - 1, 1]) || 0;
-    const yesterday = get(temp, [temp.length - 2, 1]) || 0;
-
-    return [today - yesterday, temp];
-  }, [aUSDIssuedData]);
-
-  const [todayNewAccounnt, newAccountHistory] = useMemo(() => {
-    if (!newAccountData.data || !newAccountData.data.results) return [];
-    const temp = get(newAccountData, 'data.results.0.series.0.values') as any[];
-    const today = get(temp, [temp.length - 1, 1]) || 0;
-
-    return [today, temp];
-  }, [newAccountData]);
+  const data = useDashboard();
 
   return (
     <div className={classes.root}>
       <div className={classes.item}>
         <Statistic
+          className={classes.statistic}
           title='aUSD Issued'
           value={
-            <div className={classes.value}>
-              <div>{todayAUSD}</div>
-              {aUSDIssuedHistory ? (
-                <TinyAreaChart
-                  color='#FF7788'
-                  data={aUSDIssuedHistory.map((item) => ({ date: item[0], value: item[1] }))}
-                  height={60}
-                  width={124}
-                  xField='date'
-                  yField='value'
-                />
-              ) : null}
-            </div>
+            <StatisticContent
+              lineColor='#ff7788'
+              areaColor='#fbe4e9'
+              value={data.aUSDIssued.value}
+              history={data.aUSDIssued.history}
+            />
           }
         />
-      </div>
-      <div className={classes.item}>
-        <Statistic title='Dex Daily Volume' value={'111111'} />
-      </div>
-      <div className={classes.item}>
-        <Statistic title='DOT Staked' value={'111111'} />
       </div>
       <div className={classes.item}>
         <Statistic
-          title='New Accounts'
+          className={classes.statistic}
+          title='Dex Daily Volume'
           value={
-            <div className={classes.value}>
-              <div>{todayNewAccounnt}</div>
-              {newAccountHistory ? (
-                <TinyAreaChart
-                  color='#FF7788'
-                  data={newAccountHistory.map((item) => ({ date: item[0], value: item[1] }))}
-                  height={60}
-                  width={124}
-                  xField='date'
-                  yField='value'
-                />
-              ) : null}
-            </div>
+            <StatisticContent
+              lineColor='#c39fdf'
+              areaColor='#f3ecf9'
+              value={data.dexDailyVolume.value}
+              history={data.dexDailyVolume.history}
+            />
           }
         />
       </div>
       <div className={classes.item}>
-        <Statistic title='Daily Trascation' value={'111111'} />
+        <Statistic
+          className={classes.statistic}
+          title='DOT Staked'
+          value={
+            <StatisticContent
+              lineColor='#4be1b5'
+              areaColor='#f3ecf9'
+              value={data.DOTStaked.value}
+              history={data.DOTStaked.history}
+            />
+          }
+        />
       </div>
       <div className={classes.item}>
-        <Statistic title='Total Asset Locked' value={'111111'} />
+        <Statistic
+          className={classes.statistic}
+          title='New Accounts'
+          value={
+            <StatisticContent
+              lineColor='#ff7788'
+              areaColor='#fbe4e9'
+              value={data.newAccounts.value}
+              history={data.newAccounts.history}
+            />
+          }
+        />
+      </div>
+      <div className={classes.item}>
+        <Statistic
+          className={classes.statistic}
+          title='Daily Trascation'
+          value={
+            <StatisticContent
+              lineColor='#c39fdf'
+              areaColor='#f3ecf9'
+              value={data.dailyTrascation.value}
+              history={data.dailyTrascation.history}
+            />
+          }
+        />
+      </div>
+      <div className={classes.item}>
+        <Statistic
+          className={classes.statistic}
+          title='Total Asset Locked'
+          value={
+            <StatisticContent
+              lineColor='#4be1b5'
+              areaColor='#dff9f1'
+              value={data.totalAssetLocked.value}
+              history={data.totalAssetLocked.history}
+            />
+          }
+        />
       </div>
     </div>
   );
--- packages/react-hooks/src/dashboardHomeHooks.ts
+++ packages/react-hooks/src/dashboardHomeHooks.ts
@@ -1,39 +1,70 @@
 import { Fixed18 } from '@acala-network/app-util';
 import { useMemo } from 'react';
+import { get } from 'lodash';
 
-import { useIssuance, useConstants, useStakingPoolHelper } from './index';
+import { useIssuance, useConstants, useStakingPoolHelper, useRequest } from './index';
 
 type HooksReturnType = Record<
   string,
   {
     value?: Fixed18;
-    history: {
+    history?: {
       date: number;
       value: number;
     }[];
   }
 >;
 
+const useHistory = (sql: string) => {
+  const data = useRequest(() => ({
+    method: 'GET',
+    url: `http://39.99.168.67:8086/query?q=${sql}`
+  }));
+
+  const [today, history] = useMemo(() => {
+    if (!data.data || !data.data.results) return [];
+    const temp = get(data, 'data.results.0.series.0.values') as any[];
+    const today = get(temp, [temp.length - 1, 1]) || 0;
+    const yesterday = get(temp, [temp.length - 2, 1]) || 0;
+
+    return [today - yesterday, temp?.slice(-7).map((item, index) => ({ date: index + 1, value: item[1] }))];
+  }, [data]);
+
+  return [today, history];
+};
+
 export const useDashboard = (): HooksReturnType => {
   const { stableCurrency } = useConstants();
   const audIssue = useIssuance(stableCurrency);
   const helper = useStakingPoolHelper();
 
+  const [, DOTStakedHistory] = useHistory(
+    'SELECT mean("amount") FROM "acala"."autogen"."dot-staked" WHERE time > now() - 8d AND time < now() GROUP BY time(1d)'
+  );
+
+  const [todayAUSD, aUSDIssuedHistory] = useHistory(
+    `SELECT MAX("amount") FROM "acala"."autogen"."issuance" WHERE time > now() - 8d AND time < now() AND asset = \'AUSD\' GROUP BY time(1d)`
+  );
+  const [todayNewAccounnt, newAccountHistory] = useHistory(
+    `SELECT SUM("count") FROM "acala"."autogen"."new-account" WHERE time > now() - 8d AND time < now() GROUP BY time(1d)`
+  );
+
   return useMemo(() => {
     return {
       aUSDIssued: {
-        value: audIssue,
-        history: []
+        value: audIssue?.toString(0),
+        history: aUSDIssuedHistory
       },
       dexDailyVolume: {
         history: []
       },
       DOTStaked: {
-        value: helper?.communalTotal,
-        history: []
+        value: helper?.communalTotal?.toString(0),
+        history: DOTStakedHistory
       },
       newAccounts: {
-        history: []
+        value: todayNewAccounnt?.toFixed(0),
+        history: newAccountHistory
       },
       dailyTrascation: {
         history: []
